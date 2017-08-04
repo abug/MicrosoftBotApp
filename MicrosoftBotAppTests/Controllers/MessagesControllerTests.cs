a074e11af31f9afff40ef9c81c8ab3687988a848
Anton Bugai <anton.bugai@gmail.com>
2017-08-04 18:11:08 +0300

    Added docker support and unit tests sample.

---
+++ MicrosoftBotAppTests/Controllers/MessagesControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MicrosoftBotApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Bot.Connector;
+
+namespace MicrosoftBotApp.Tests
+{
+    [TestClass]
+    public class MessagesControllerTests
+    {
+        [TestMethod]
+        public void PostTest()
+        {
+            var controller = new MessagesController
+            {
+                Request = new HttpRequestMessage()
+            };
+
+            Activity activity = new Activity();
+
+            var result = controller.Post(activity).Result;
+
+            Assert.Fail("Sample assert.");
+        }
+    }
+}
